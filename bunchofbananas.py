c9381b44100da46250c245e4c3e089ab54049a09
Hannahgolluri <hannahgolluri@gmail.com>
2026-05-27 14:30:35 -0500

    added bunchofbananas program

---
+++ bunchofbananas.py
@@ -0,0 +1,8 @@
+bananas = 5
+
+if bananas == 0:
+    print("I have no bananas")
+elif bananas >= 1 and bananas <= 4:
+    print("Small bunch of bananas")
+else:
+    print("I have a bunch of bananas")
