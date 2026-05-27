979c8703bde1f2d706d7102b813e08e5d4d4e07b
Hannahgolluri <hannahgolluri@gmail.com>
2026-05-27 15:20:58 -0500

    added dictionary program

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Hannah",
+    "age": 25,
+    "city": "Overland"
+}
+
+print(student)
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("City:", student["city"])
